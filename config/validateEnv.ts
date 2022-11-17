a25ab6b466b84542b0a01031bc3738404f4f315e
Kai Krings <kai.krings@posteo.de>
2022-11-17 06:27:21 +0000

    Remove schema.ts

--- config/validateEnv.ts
+++ config/validateEnv.ts
@@ -1,4 +1,14 @@
-import schema from './schema';
+import Joi from 'joi';
+
+interface Schema {
+  WELTENBUMMLERPAAR_FRONTEND_API_BASE_URL: string;
+  WELTENBUMMLERPAAR_FRONTEND_IMG_BASE_URL: string;
+}
+
+const schema = Joi.object<Schema>({
+  WELTENBUMMLERPAAR_FRONTEND_API_BASE_URL: Joi.string().uri().required(),
+  WELTENBUMMLERPAAR_FRONTEND_IMG_BASE_URL: Joi.string().uri().required(),
+});
 
 export function validateEnv(vars: Record<string, string>) {
   const { error } = schema.validate(vars);

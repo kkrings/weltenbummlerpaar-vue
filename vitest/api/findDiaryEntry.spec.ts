2b1ac20d79a0f45fa9997ff538ab7f43f186cfc7
Kai Krings <kai.krings@posteo.de>
2022-11-24 09:04:29 +0000

    Add more test cases to findDiaryEntry.spec.ts

--- vitest/api/findDiaryEntry.spec.ts
+++ vitest/api/findDiaryEntry.spec.ts
@@ -3,12 +3,26 @@ import { setupServer, type SetupServerApi } from 'msw/node';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { findDiaryEntry } from '@/api/findDiaryEntry';
 import type { DiaryEntry } from '@/types/DiaryEntry';
+import type { Image } from '@/types/Image';
 
 const baseUrl = import.meta.env.WELTENBUMMLERPAAR_FRONTEND_API_BASE_URL;
 
+function mockBackend(diaryEntry: DiaryEntry) {
+  const url = `${baseUrl}/diary-entries/${diaryEntry.id}`;
+
+  const handler = rest.get(url, (_req, res, ctx) => {
+    return res(ctx.json(diaryEntry));
+  });
+
+  const server = setupServer(handler);
+  server.listen({ onUnhandledRequest: 'error' });
+
+  return server;
+}
+
 describe('findDiaryEntry', () => {
   let diaryEntry: DiaryEntry;
-  let url: string;
+  let backend: SetupServerApi;
 
   beforeEach(() => {
     diaryEntry = {
@@ -23,31 +37,44 @@ describe('findDiaryEntry', () => {
     };
   });
 
-  beforeEach(() => {
-    url = `${baseUrl}/diary-entries/${diaryEntry.id}`;
-  });
-
   describe('diary entry', () => {
-    let server: SetupServerApi;
-
     beforeEach(() => {
-      const handler = rest.get(url, (_req, res, ctx) => {
-        return res(ctx.json(diaryEntry));
-      });
-
-      server = setupServer(handler);
-    });
-
-    beforeEach(() => {
-      server.listen({ onUnhandledRequest: 'error' });
+      backend = mockBackend(diaryEntry);
     });
 
     it('should have been found', async () => {
       expect(await findDiaryEntry(diaryEntry.id)).toEqual(diaryEntry);
     });
+  });
 
-    afterEach(() => {
-      server.close();
+  describe('diary entry with date range and preview image', () => {
+    beforeEach(() => {
+      diaryEntry.dateRange = {
+        dateMin: new Date(2022, 11, 16),
+        dateMax: new Date(2022, 11, 17),
+      };
+
+      const previewImage: Image = {
+        id: '637f30223aec94e94d7226e2',
+        description: 'some description',
+        createdAt: new Date(2022, 11, 17),
+        updatedAt: new Date(2022, 11, 17),
+      };
+
+      diaryEntry.images.push(previewImage);
+      diaryEntry.previewImage = previewImage;
     });
+
+    beforeEach(() => {
+      backend = mockBackend(diaryEntry);
+    });
+
+    it('should have been found', async () => {
+      expect(await findDiaryEntry(diaryEntry.id)).toEqual(diaryEntry);
+    });
+  });
+
+  afterEach(() => {
+    backend.close();
   });
 });
